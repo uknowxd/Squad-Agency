c5a318194cab4c0e0d02a36d625dbd8173319f4c
uknowxd <kitisaksrisuk@gmail.com>
2023-01-03 21:07:53 +0700

    3-1-2023

---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'adamant'
+games { 'gta5' }
+
+author 'uknowxd#5513'
+description 'Squad-Agency By: Qwerty-Hub'
+
+server_scripts { 'core/sv.lua' , 'config.lua' }
+client_scripts { 'core/cl.lua' , 'config.lua' }
